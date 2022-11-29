51231248bbf6dde88e490e3cf60b0c03cc6b09a8
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-29 23:59:06 +0000

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str_cpy = str[:n] + str[n + 1:]
-    return (str_cpy)
+    if n >= 0:
+        str_cpy = str[:n] + str[n + 1:]
+        return (str_cpy)
+    else:
+        return (str)
