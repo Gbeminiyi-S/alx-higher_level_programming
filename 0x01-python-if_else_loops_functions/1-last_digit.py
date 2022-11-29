d8da2081235913632cd9761b3e51c89605aee973
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-29 23:06:07 +0000

    update for line too long

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,10 @@ if number < 0:
     remainder = -1 * (abs(number) % 10)
 else:
     remainder = number % 10
+print(f"Last digit of {number} is", end=' ')
 if remainder > 5:
-    print(f"Last digit of {number} is {remainder} and is greater than 5")
+    print(f"{remainder} and is greater than 5")
 elif remainder == 0:
-    print(f"Last digit of {number} is {remainder} and is 0")
+    print(f"{remainder} and is 0")
 else:
-    print(f"Last digit of {number} is {remainder} and is less than 6 and not 0")
+    print(f"{remainder} and is less than 6 and not 0")
