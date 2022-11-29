99324f250ee263bc2cd8ff389a98d5da3c8c8914
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-29 23:01:15 +0000

    update for indentation error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	remainder = -1 * (abs(number) % 10)
+    remainder = -1 * (abs(number) % 10)
 else:
-	remainder = number % 10
+    remainder = number % 10
 if remainder > 5:
-	print(f"Last digit of {number} is {remainder} and is greater than 5")
+    print(f"Last digit of {number} is {remainder} and is greater than 5")
 elif remainder == 0:
-	print(f"Last digit of {number} is {remainder} and is 0")
+    print(f"Last digit of {number} is {remainder} and is 0")
 else:
-	print(f"Last digit of {number} is {remainder} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {remainder} and is less than 6 and not 0")
