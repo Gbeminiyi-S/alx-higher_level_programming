40412f12e09fe7c24fdcf21233db6986bf4ae6bf
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-12-01 22:54:02 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
