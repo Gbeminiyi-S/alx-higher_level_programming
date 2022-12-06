d4ab586b90f69f0cc1d07eb1f323ff10291644a2
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-12-06 23:25:28 +0000

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if type(matrix) == list:
+        for row in matrix:
+            separator = ''
+            for element in row:
+                if separator != '':
+                    print(separator, end='')
+                print("{}{:x}".format(separator, element), end='')
+                separator = ' '
+            print()
