dca23ea897c18d6805136173cb8f671d276595ec
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-12-19 21:48:14 +0000

    Task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        quotient = 0
+        try:
+            quotient = my_list_1[i] / my_list_2[i]
+        except (ValueError, TypeError):
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(quotient)
+    return result
