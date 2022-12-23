6fed5e7cd79dbf8355aef29790a039017442551b
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2022-12-23 21:34:54 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""No module imported"""
+
+
+class Square:
+    """Defines a class: Square with a private attribute: size"""
+
+    def __init__(self, size):
+        self.__size = size
