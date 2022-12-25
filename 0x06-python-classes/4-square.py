fd276c3d9ecde2382c92230c2955946319135d3b
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2022-12-25 23:07:18 +0100

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""No module imported"""
+"""Defines a class 'Square'"""
 
 
 class Square:
-    """Defines a class: Square with a private attribute: size and checks Errors
-       and returns the area of the square"""
+    """Represents a square"""
 
     def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+            size (int): size of new square
+        """
+
         self.size = size
 
     def area(self):
+        """Returns the area of instance of square"""
         return (pow(self.__size, 2))
 
     @property
     def size(self):
+        """Gets the current size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Sets the current size of the square."""
         if isinstance(value, int):
             self.__size = value
         else:
