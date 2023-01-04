c448887addd9a95a8e4c515e20715613ed70be11
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-01-04 07:23:21 +0000

    updated by removing whitespaces

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@
 
 def add_integer(a, b=98):
     """ Returns the sum of ints a and b
-    
+
         If a / b is a float, typecast to an integer """
 
     if not isinstance(a, (int, float)):
@@ -16,5 +16,5 @@ def add_integer(a, b=98):
 
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
