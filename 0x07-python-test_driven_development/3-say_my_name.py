61043a6298f995db89bdf5b12998f6afaf2025b4
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-01-04 09:25:42 +0000

    update to remove whitespaces

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,11 +4,11 @@
 
 def say_my_name(first_name, last_name=""):
     """ prints a person's full name
-    
+
     Args:
         first_name (str): The person's first name
         last_name (str): The person's last name
-    
+
     Raises:
         TypeError: If either of first_name or last_name are not strings
     """
