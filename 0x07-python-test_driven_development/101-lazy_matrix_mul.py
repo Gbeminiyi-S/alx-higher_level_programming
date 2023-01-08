a7c16e5a8cb421e87acdea015acd1ae3fecd6048
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-08 07:37:31 +0000

    Task 7

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,4 +10,4 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first matrix
         m_b (list of lists of ints/floats): The second matrix
     """
-    return np.dot(m_a, m_b)
+    return np.matmul(m_a, m_b)
