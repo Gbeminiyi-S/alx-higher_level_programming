f6a83cab4b1447af44ce7fa91430c5bef4e57f9f
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-11 17:06:49 +0000

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module to check available attributes
+and methods of an object
+"""
+
+
+def lookup(obj):
+    """ Returns the attributes and methods """
+
+    return dir(obj)
