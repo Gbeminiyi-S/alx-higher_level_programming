01091a8cb17e3ae0d21dcd03067d1c92d0400dbf
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-12 10:56:25 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module that creates a class: BaseGeometry """
+
+
+class BaseGeometry:
+    """ defines the class: BaseGeometry """
+
+    def area(self):
+        """ returns the area """
+
+        raise Exception('area() is not implemented')
