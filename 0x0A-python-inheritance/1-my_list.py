2c2f565c2810911126869605bb0999ba998883e5
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-12 09:43:21 +0000

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module to create inherited class """
+
+class MyList(list):
+    """ creates a new class, MyList that inherits from list """
+
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort) """
+
+        print(sorted(self))
