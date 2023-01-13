2309702d9d41eefa5b83f4f1dbc6e911583fcb9b
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-13 13:47:57 +0000

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module that creates a subclass: Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ defines a subclass: Square """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
+    def area(self):
+        """ returns the area of the square """
+
+        return pow(self.__size, 2)
+
+    def __str__(self):
+        return "[Square] " + str(self.__size) + '/' + str(self.__size)
