5d15fd4408577bf7e3e367c12f323b19c5d33845
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-13 13:09:47 +0000

    update from isinstance to type

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """ validates value """
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(name + ' must be an integer')
 
         if value <= 0:
