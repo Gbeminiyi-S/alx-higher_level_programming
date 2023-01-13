b578fbac1e9e4c2ac9f22ea28224adb17d9453c2
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-13 13:06:14 +0000

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module that creates a class: BaseGeometry """
+
+
+class BaseGeometry:
+    """ defines the class: BaseGeometry """
+
+    def area(self):
+        """ returns the area """
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ validates value """
+
+        if not isinstance(value, int):
+            raise TypeError(name + ' must be an integer')
+
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
