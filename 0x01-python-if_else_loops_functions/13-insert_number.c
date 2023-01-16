0be40ab3e1b86d1888b75f97f15a479e2c558911
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-16 12:27:28 +0000

    update for empty list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,12 +13,16 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp = *head, *new_node = NULL, *prev;
 
-	if (*head)
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+
+	if (!(*head))
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (!new_node)
-			return (NULL);
-		new_node->n = number;
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
 	}
 
 	if ((*head)->n >= number)
