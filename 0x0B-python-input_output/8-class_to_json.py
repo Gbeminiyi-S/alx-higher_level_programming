ecfd6e18188eaec70f0ee5d91cd8893448e592aa
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-20 14:40:28 +0000

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module that creates a function: class_to_json """
+
+
+def class_to_json(obj):
+    """ returns the dictionary description with simple data structure
+        (list, dictionary, string, integer and boolean) for JSON
+        serialization of an object """
+
+    return obj.__dict__
