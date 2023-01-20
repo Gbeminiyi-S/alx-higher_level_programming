09344c71fcfaef542a27677bda7f137bcc7ca048
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-20 10:44:18 +0000

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to create a class: read_file """
+
+
+def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end='')
