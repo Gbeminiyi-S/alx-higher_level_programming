a5508967f2d4e467b5eaaa7bc3b81ccb3c09471c
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-20 11:37:20 +0000

    update to remove whitespace

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ writes an Object to a text file, using a JSON representation """
-    
+
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
