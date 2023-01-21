55c46e15ccb2de9c858e6cc0c9ca7481befa49ea
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-21 19:29:11 +0000

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A script that add all arguments to a Python list and save them to a file
+"""
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    items = load_from_json_file("add_item.json")
+    for item in sys.argv[1:]:
+        items.append(item)
+except FileNotFoundError:
+    items = []
+save_to_json_file(items, "add_item.json")
