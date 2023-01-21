60add8a304ec4a8c8c19ce7464d95e8483905b16
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-21 19:53:13 +0000

    Task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -16,7 +16,7 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, encoding="utf-8") as f:
         for line in f:
             new_text += line
-            if new_string in line:
+            if search_string in line:
                 new_text += new_string
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(new_text)
