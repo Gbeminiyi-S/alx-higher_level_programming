fdea35af69c62ca71dbe78282ac05d6ee7297d17
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-01-21 07:07:39 +0100

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,5 +8,4 @@ def append_write(filename="", text=""):
     """
 
     with open(filename, 'a', encoding="utf-8") as f:
-        f.write(text)
-        return len(text)
+        return f.write(text)
