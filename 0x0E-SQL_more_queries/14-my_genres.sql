94b00e41572f004a2792e5ea5f1cdeae531d3330
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-02-18 15:55:36 +0000

    Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,5 +4,5 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter'
+WHERE title = 'Dexter'
 ORDER BY name ASC;
