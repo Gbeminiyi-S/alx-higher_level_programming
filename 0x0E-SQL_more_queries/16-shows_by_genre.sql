d03f666c44db42636c7eb126d8cdca587381e3ba
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-02-18 16:07:48 +0000

    Task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT title, name FROM tv_genres
-LEFT JOIN tv_show_genres
+RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
+RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title, name ASC;
