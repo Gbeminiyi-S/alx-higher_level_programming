e4567bdef40e316af19d767c780aa9809564b424
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-02-18 21:09:18 +0100

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- creates the database hbtn_0d_usa and the table cities on MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+	FOREIGN KEY(state_id) REFERENCES states(id)
 );
