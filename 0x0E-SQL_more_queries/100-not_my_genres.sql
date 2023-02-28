01b5b371def17be2f5adac9790eda2089a4b3e1d
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-02-28 21:14:22 +0000

    Task 17 updated

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,4 +6,5 @@ WHERE g.name NOT IN (
 	SELECT DISTINCT name FROM tv_genres AS g
 	INNER JOIN tv_show_genres AS tg ON tg.genre_id = g.id
 	INNER JOIN tv_shows AS s ON s.id = tg.show_id
-	WHERE title = 'Dexter');
+	WHERE title = 'Dexter')
+ORDER BY name;
