1db022991420d05ede2314e30bd3aa69bd4bda03
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-13 17:17:58 +0000

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const itr = Number(process.argv[2]);
+if (isNaN(itr)) {
+  console.log('Missing number of occurrences');
+}
+else {
+  for (let i = 0; i < itr; i++) {
+    console.log('C is fun');
+  }
+}
