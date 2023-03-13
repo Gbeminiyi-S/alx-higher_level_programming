9f1b9dbfc891b78672f02e3243f9fb50a73dbd53
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-03-13 22:41:42 +0100

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const num1 = Number(process.argv[2]);
-const num2 = Number(process.argv[3]);
-console.log(`${num1 + num2}`);
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+function add(a, b) {
+  console.log(a + b);
+}
