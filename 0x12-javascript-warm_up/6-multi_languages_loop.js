95cffd225c49fdba2e68403377a0d432c95b67cf
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-15 09:17:47 +0000

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let element of stringArray) {
+for (const element of stringArray) {
   console.log(element);
 }
