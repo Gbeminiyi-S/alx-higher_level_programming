c634a7195622b5fcc46c6f7f089f861a2b4c5016
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-03-15 10:12:21 +0100

    Task 6
    Updated for loop with for...of

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < stringArray.length; i++) {
-  console.log(`${stringArray[i]}`);
+for (let element of stringArray) {
+  console.log(element);
 }
