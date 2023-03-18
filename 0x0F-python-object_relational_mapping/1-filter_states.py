e25d8ac411842cb5c4f312d6196d1a306e3a357d
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-18 00:13:19 +0000

    Adding BINARY to the name query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%';")
     states = cursor.fetchall()
     for state in states:
         print(state)
