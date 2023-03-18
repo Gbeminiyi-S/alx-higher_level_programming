fd7e16fd4b9b6ad1b196cb092c5ea16de6562a8e
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-18 15:31:15 +0000

    Task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a script that lists all `cities` from the database `hbtn_0e_4_usa`
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT c.id, c.name, s.name FROM cities as c\
+                    inner join states as s\
+                    ON c.state_id = s.id")
+    cities = cursor.fetchall()
+
+    for city in cities:
+        print(city)
