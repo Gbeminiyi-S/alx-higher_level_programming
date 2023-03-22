aebb0d4a25031ef6b22319c8585450b7e4520a60
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-22 22:27:27 +0000

    Task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     states = session.query(State).order_by(State.id)
 
     for state in states:
