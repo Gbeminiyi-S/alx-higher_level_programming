45740ad806f652840e662b095846358204833500
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-30 21:19:45 +0000

    Task 0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+# Get the size of the body of the response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
