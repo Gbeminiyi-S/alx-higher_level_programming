36c8c64ef0a6ac810cde4381d5547e030851cbd5
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-31 10:56:38 +0000

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Takes in a URL and an email, sends a POST request to the passed URL
+    with the email as a parameter, and displays the body of the response
+    (decoded in utf-8)
+"""
+import urllib.parse
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+value = {'email': sys.argv[2]}
+
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    print(response.read().decode("utf-8"))
