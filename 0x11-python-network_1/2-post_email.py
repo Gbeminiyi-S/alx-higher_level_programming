4b8c9a7254a83a5f006eb1de5c694f6ae5168b02
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-31 11:18:52 +0000

    update to prevent exec when imported

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,12 +7,12 @@ import urllib.parse
 import urllib.request
 import sys
 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
 
-url = sys.argv[1]
-value = {'email': sys.argv[2]}
-
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode("utf-8"))
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
