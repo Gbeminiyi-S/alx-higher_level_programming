12c81396e417c3c0b2584b97b5caf31fb0006a6d
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-31 12:17:13 +0000

    update to prevent exec on import

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,6 @@
 import requests
 import sys
 
-
-req = requests.get(sys.argv[1]).headers
-print(req.get("X-Request-Id"))
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1]).headers
+    print(req.get("X-Request-Id"))
