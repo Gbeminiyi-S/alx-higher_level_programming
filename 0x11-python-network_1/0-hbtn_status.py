8cbaf7e5e0525dade7f27d935a92ebdbf0f7c496
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-03-31 11:29:28 +0000

    update to order of printing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
-import urllib
+import urllib.request
 
 
 if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
