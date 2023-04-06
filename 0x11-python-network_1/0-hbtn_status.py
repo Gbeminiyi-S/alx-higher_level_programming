3aad3555bc38004a8262e4d5c12a2106564960ed
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-04-06 16:40:49 +0000

    update for check 5

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetches a url using urllib"""
+"""Fetches a url using urllib
 import urllib.request
 
 
@@ -9,4 +9,16 @@ if __name__ == "__main__":
     print("Body response:")
     print(f"\t- type: {type(html)}")
     print(f"\t- content: {html}")
-    print(f"\t- utf8 content: {html.decode('utf-8')}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")"""
+
+'''a python script that fetches a url using urllib'''
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print(f'\t- content: {content}')
+    print(f'\t- utf8 content: {content.decode("utf-8")}')
