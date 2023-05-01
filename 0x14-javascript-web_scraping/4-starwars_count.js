646c9f24ae3788ebded621d97906fd455e13da00
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-05-01 01:19:21 +0000

    Task 4 - Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints the number of movies where the character “Wedge Antilles” is present
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  const numFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')).length;
+
+  console.log(numFilms);
+});
