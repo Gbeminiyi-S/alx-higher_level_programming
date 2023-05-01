8a5c18a9066e85bd56dce6e3be596103afb69dc2
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-05-01 00:28:27 +0000

    Task 2 - update to add error case

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
+  if (response) {
     console.log(`code: ${response.statusCode}`);
+  } else {
+    console.log(error);
+  }
 });
