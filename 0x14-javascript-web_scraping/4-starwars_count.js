a0c66b2289aa3c09f152de3e225dceb224c0eec2
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-05-01 07:49:21 +0000

    Task 4 - update to the syntax

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,13 +3,16 @@
 const request = require('request');
 
 const url = process.argv[2];
+const characterId = '18';
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const numFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')).length;
-    console.log(`${numFilms}`);
+    return;
   }
+
+  const films = JSON.parse(body).results;
+  const numFilms = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+
+  console.log(`${numFilms}`);
 });
